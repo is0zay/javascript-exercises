514c6363c045d6bcadf95cea82222e1f60f34a4f
Isaiah <ieshembo@alumni.uncg.edu>
2022-07-22 12:18:32 -0400

    Completed but still needs practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+   const array = args[0];
+   return array.filter(remove => !args.includes(remove));
+   
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
