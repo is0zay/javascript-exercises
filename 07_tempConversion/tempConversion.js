b7e0150e9907195564cc994d45d20c07a028f5aa
Isaiah <ieshembo@alumni.uncg.edu>
2022-07-22 13:56:07 -0400

    temp conversion passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let newCTemp = (ftemp - 32) * 5/9;
+  return Math.round(newCTemp);
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let newFTemp = (ctemp * 9/5) + 32;
+  return Math.round(newFTemp);
 };
 
 // Do not edit below this line
