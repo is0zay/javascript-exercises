96e42a064956c827e78c0546e120e4b657938db9
Isaiah <ieshembo@alumni.uncg.edu>
2022-08-02 15:47:43 -0400

    exercise complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a , b) {
+	const sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	const total = [].reduce((a, b) => a + b, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  const product = [].reduce((a, b) => a * b, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const answer = Math.pow(a,b);
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let runningTotal = 0
+
+  if (num === 0) { return 1;}
+  else {
+    for (i = num; i> 0; i--) {
+      toAdd = i * (num - 1);
+      return runningTotal += toAdd;
+    }
+  }
 };
 
 // Do not edit below this line
