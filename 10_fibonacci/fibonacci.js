405143ffaf0cfb0d644ac2afbc6ec9b04ce5a6fa
Isaiah <ieshembo@alumni.uncg.edu>
2022-08-02 21:37:20 -0400

    complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = parseInt(n);
+   
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+
+    let a= 0;
+    let b = 1;
+
+    for (let i = 1; i < num; i++) {
+        const count = b;
+        b = a + b;
+        a = count;
+    }
+    
+    return b;
 
 };
 
