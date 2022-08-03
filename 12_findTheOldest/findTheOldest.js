e6aa0631f86396712818a0f63b25658315a698e0
Isaiah <ieshembo@alumni.uncg.edu>
2022-08-03 11:01:17 -0400

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest =  people.reduce((a , b) => {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return next > last ? b : a;
+    }); return oldest;
 };
 
 // Do not edit below this line
